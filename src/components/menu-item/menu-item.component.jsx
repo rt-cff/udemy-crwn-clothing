31d38402c56edaefeb34011e6f075c1897324de3
Raymond Tam <raymondtamtwtp@gmail.com>
2020-02-01 17:06:48 +0800

    to upper case for title

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -10,7 +10,7 @@ const MenuItem  = ({title, size, imageUrl, linkUrl, history, match}) => (
     >
         <div style = {{background: `url(${imageUrl})`}} className = 'background-image'/>
         <div className = 'content'>
-            <h1 className = 'title'>{title}</h1>
+            <h1 className = 'title'>{title.toUpperCase()}</h1>
             <span className = 'subtitle'>SHOP NOW</span>
         </div>
     </div>
