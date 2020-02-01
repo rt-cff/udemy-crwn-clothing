8fdafb0aec81b3a9ffd98df4b3d8f6bfd11bc5bb
Raymond Tam <raymondtamtwtp@gmail.com>
2020-02-01 17:38:20 +0800

    create header compnent with SHOP, Contact Link on top

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -11,6 +13,7 @@ const Hats = () => (<h1>HATS</h1>)
 function App() {
   return (
     <div className="App">
+      <Route path = '/' component = {Header}/>
       <Switch>
         <Route exact path = '/' component = {HomePage}/>
         <Route exact path = '/shop' component = {ShopPage}/>
