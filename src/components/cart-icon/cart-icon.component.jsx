73bfeb9013a2666df139fca084d5892ae8912258
Raymond Tam <raymondtamtwtp@gmail.com>
2020-03-06 17:05:22 +0800

    update cart item count with reselect library for memozied reduce function

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selector'
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
@@ -17,8 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()), 
 })
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    count: cartItems.length
+const mapStateToProps = (state) => ({
+    count: selectCartItemsCount(state), 
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
