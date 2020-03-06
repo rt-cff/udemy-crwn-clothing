238e2beb7e5d1007cec33c82d0db88355de4d9dc
Raymond Tam <raymondtamtwtp@gmail.com>
2020-03-06 22:29:39 +0800

    Add Checkout Page & Implement Checkout Item Component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
@@ -59,6 +60,7 @@ class App extends React.Component {
           <Route exact path = '/' component = {HomePage}/>
           <Route exact path = '/shop' component = {ShopPage}/>
           <Route exact path = '/hats' component = {Hats}/>
+          <Route exact path = '/checkout' component = {CheckoutPage}/>
           {/*<Route exact path = '/signin' component = {SignInAndSignUpPage}/>*/}
           <Route exact path = '/signin'>
             {this.props.loggedIn ? <Redirect to = '/'/> : <SignInAndSignUpPage/>}
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -16,3 +16,8 @@ export const selectCartHidden = createSelector(
     [selectCart], 
     cart => cart.hidden
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems], 
+    cartItems => cartItems.reduce((count, cartItem) => count + cartItem.price * cartItem.quantity, 0)
+)
