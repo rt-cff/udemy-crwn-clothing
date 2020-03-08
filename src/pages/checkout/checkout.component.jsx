1c2907d72c23d5c13e24762dccec038c3f72ac63
Raymond Tam <raymondtamtwtp@gmail.com>
2020-03-09 00:01:31 +0800

    integrate stripe for payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect'
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selector'
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import './checkout.styles.scss'
 
@@ -30,7 +31,13 @@ const CheckoutPage = ({items, total}) => (
         {items.map(item => (
                 <CheckoutItem key = {item.id} item = {item}/>
             ))}
-        <div className = 'total'>{total}</div>
+        <div className = 'total'>TOTAL: ${total}</div>
+        <div className = 'test-warning'>
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/20 - CVV:123
+        </div>
+        <StripeCheckoutButton />
     </div>
 )
 
